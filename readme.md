bee5ce65bb7522170676d38950bf21ebcb189533
Maria Gnedina <elagina.maria.n@gmail.com>
2021-04-08 19:15:32 +0300

    13.link preview

---
+++ readme.md
@@ -0,0 +1,1 @@
+This is a tutorial from https://github.com/drucoder/sarafan
--- src/main/java/letscode/sarafan/domain/Message.java
+++ src/main/java/letscode/sarafan/domain/Message.java
@@ -2,6 +2,7 @@ package letscode.sarafan.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table
+@Data
 @ToString(of = {"id", "text"})
 @EqualsAndHashCode(of = {"id"})
 public class  Message {
@@ -26,27 +28,12 @@ public class  Message {
     @JsonView({Views.FullMessage.class})
     private LocalDateTime creationDate;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public LocalDateTime getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
+    @JsonView(Views.FullMessage.class)
+    private String link;
+    @JsonView(Views.FullMessage.class)
+    private String linkTitle;
+    @JsonView(Views.FullMessage.class)
+    private String linkDescription;
+    @JsonView(Views.FullMessage.class)
+    private String linkCover;
 }
