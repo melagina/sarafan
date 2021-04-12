f6461c03e87001fa5a2b73c43d497dd8af248570
Maria Gnedina <elagina.maria.n@gmail.com>
2021-04-12 15:34:53 +0300

    16. lazy load

---
+++ src/main/resources/static/js/components/LazyLoader.vue
@@ -0,0 +1,29 @@
+<template>
+    <span></span>
+</template>
+
+<script>
+    import { mapActions } from 'vuex'
+
+    export default {
+        name: 'LazyLoader',
+        methods: mapActions(['loadPageAction']),
+        mounted() {
+            window.onScroll = () => {
+                const el = document.documentElement
+                const isBottomOfScreen = el.scrollTop + window.innerHeight === el.offsetHeight
+
+                if (isBottomOfScreen) {
+                    this.loadPageAction()
+                }
+            }
+        },
+        beforeDestroy() {
+            window.onScroll = null
+        }
+    }
+</script>
+
+<style>
+
+</style>
